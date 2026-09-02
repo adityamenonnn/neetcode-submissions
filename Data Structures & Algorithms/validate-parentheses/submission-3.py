912deae206707d4d019676669563d21c8609756b
adityamenonnn <menonadityaaltis@gmail.com>
2026-09-02 23:58:40 +0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        match = {'}':'{',')':'(',']':'[' }
+
+        stack = []
+
+        for i in s:
+            if i in match:
+                if stack and stack[-1]==match[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+            
+        if not stack:
+            return True
+        else:
+            return False
